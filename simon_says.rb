aba62df10a19e9de02c0523c23f794f5b80b70b4
Billy Davidson <billy.davids52@gmail.com>
2018-08-29 12:40:20 -0400

    complete

---
+++ simon_says.rb
@@ -0,0 +1,26 @@
+# class SimonSays
+  def echo(string)
+    string
+  end
+
+  def shout(string)
+    string.upcase
+  end
+
+  def repeat(string, integer)
+    greet = (string + " ") * integer
+    greet.chop
+  end
+
+  def start_of_word(string, integer)
+    string.slice(integer)
+  end
+
+  def return_first_two_letters(string, integer)
+    string.(integer)
+  end
+
+  def first_word(string)
+    string.split[0]
+  end
+# end
